21f5ec47f6e6f69f0690a6e4fd26f076c7a26b6b
Asus <Asus@DESKTOP-IJEPJQM>
2023-06-19 12:36:00 +0400

    sort iteration

--- main.js
+++ main.js
@@ -93,17 +93,80 @@
 
 /*FIND INDEX ITERATION */
 
-const numbers = [7, 33, 45, 78, 2, 15, 99];
-let addNumber = 85;
+// const numbers = [7, 33, 45, 78, 2, 15, 99];
+// let addNumber = 85;
 
-findIndexIteration(numbers, addNumber);
+// findIndexIteration(numbers, addNumber);
 
-function findIndexIteration(numbers, addNumber) {
-    for(let i = 0; i < numbers.length; i++) {
-        if(numbers[i] > addNumber) {
-            alert("index of first number over your number is " + i);
-            return;
+// function findIndexIteration(numbers, addNumber) {
+//     for(let i = 0; i < numbers.length; i++) {
+//         if(numbers[i] > addNumber) {
+//             alert("index of first number over your number is " + i);
+//             return;
+//         }
+//     }
+//     alert("not any value over your value");
+// }
+
+/*SORT ITERATION */
+
+let array = ["Jamil", "Amrah", "Farrukh", "Hilal"];
+sortIteration(array);
+
+function sortIteration(array) {
+    for(let i = 0; i < array.length-1; i++) {
+        let firstDataOfArray = array[i];
+        let arrayIndex = [];
+        let firstTextIndex = findLetterIndex(firstDataOfArray);
+        arrayIndex.push(firstTextIndex);
+
+        for(let r = i+1; true; ) {
+            let nextDataOfArray = array[r];
+            let arrayIndex = [];
+            let secondTextIndex = findLetterIndex(nextDataOfArray);
+            arrayIndex.push(secondTextIndex);
+            checkIndex(array, firstTextIndex, secondTextIndex)
+
+            function checkIndex(array, firstTextIndex, secondTextIndex) {
+                if(firstTextIndex.length <= secondTextIndex.length) {
+                    for(let f = 0; f < firstTextIndex.length; f++) {
+                        if(firstTextIndex[f] < secondTextIndex[f]) {
+                            let temp = array[i];
+                            array[i] = array[r];
+                            array[r] = temp;
+                            break
+                        }
+                    }
+                }
+                else {
+                    for(let s = 0; s < secondTextIndex.length; s++) {
+                        if(firstTextIndex[s] < secondTextIndex[s]) {
+                            let temp = array[i];
+                            array[i] = array[r];
+                            array[r] = temp;
+                            break;
+                        }
+                    }
+                }
+            }
+            break;
         }
     }
-    alert("not any value over your value");
+    
+    function findLetterIndex(firstDataOfArray) {
+        let uppercaseLetters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
+        let lowerLetters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+    
+        let index = [];
+    
+        for(let j = 0; j < firstDataOfArray.length; j++) {
+           let letter = firstDataOfArray[j];
+           for(let k = 0; k < uppercaseLetters.length; k++) {
+               if(letter == uppercaseLetters[k] || letter == lowerLetters[k]) {
+                    index.push(k);
+               }
+           }
+        }
+        return index;
+    }
 }
